97256cc57ca6b07343bf6875cf6a6fbc631612c6
Andrew Megalaa <andrewmuhareb@gmail.com>
2024-03-07 15:49:42 -0500

    dev init

--- release.sh
+++ release.sh
@@ -20,25 +20,31 @@ if [ ! -f "$CONFIG" ]; then
     exit 1
 fi
 
-git fetch --all
-git checkout $DEV 
-git merge $MASTER --no-edit
-
-poetry version $VERSION_TYPE
 
 VERSION="$(poetry version -s)"
-echo $VERSION
-parse-changelog --changelog CHANGELOG.md --release $VERSION
-pre-commit run --all-files
-git commit -am "$VERSION"
-git tag -a v$VERSION -m "v$VERSION"
-git checkout $MASTER 
-git merge $DEV --no-edit
-poetry build
-git push origin --tags
-git push origin dev
-git push origin master
-USERNAME=$(awk '{print $1}' $CONFIG)
-PASSWORD=$(awk '{print $2}' $CONFIG)
-poetry publish --username $USERNAME --password $PASSWORD
-git checkout $DEV
+read -p "Performing $VERSION_TYPE upgrade from $VERSION: Type yes to continue:" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY == "yes"]]
+then
+    git fetch --all
+    git checkout $DEV
+    git merge $MASTER --no-edit
+
+    poetry version $VERSION_TYPE
+
+    echo $VERSION
+    parse-changelog --changelog CHANGELOG.md --release $VERSION
+    pre-commit run --all-files
+    git commit -am "$VERSION"
+    git tag -a v$VERSION -m "v$VERSION"
+    git checkout $MASTER
+    git merge $DEV --no-edit
+    poetry build
+    git push origin --tags
+    git push origin dev
+    git push origin master
+    USERNAME=$(awk '{print $1}' $CONFIG)
+    PASSWORD=$(awk '{print $2}' $CONFIG)
+    poetry publish #--username $USERNAME --password $PASSWORD
+    git checkout $DEV
+fi
